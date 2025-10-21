173f124ce5df1c022373bf1b5b74b2b74c45a9b9
keitaphotocamera-tech <keitaphotocamera@gmail.com>
2025-10-21 21:08:50 +0700

    Delete src directory

--- src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-// 🔹 ใส่ค่า URL และ KEY ของโปรเจ็กต์คุณจาก https://app.supabase.com
-const SUPABASE_URL = "https://rkfnljzdcqpusfqgwewl.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrZm5sanpkY3FwdXNmcWd3ZXdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwNDk0MjcsImV4cCI6MjA3NjYyNTQyN30.tXCvcE22Nu9xEFuuoOP6DGZ1DvLijLsPKvyi64orydM";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
