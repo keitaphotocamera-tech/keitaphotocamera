f11ce83095df4d34df9cfe0fda634d80bf85c413
keitaphotocamera-tech <keitaphotocamera@gmail.com>
2026-05-16 02:52:08 +0700

    Update config.js

--- photo-store/js/config.js
+++ photo-store/js/config.js
@@ -3,14 +3,20 @@ window.CONFIG = {
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0bWxtaWl5bGd0dGJlaHlmampkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NTc5NjAsImV4cCI6MjA5NDQzMzk2MH0.O1EUG0MOGUZEL5WOVlZYS_hfUgSsGEzTUL69Nm1PWgo",
   LIFF_ID: "1661401303-Wld4uwIt",
 
-  DEFAULT_PHOTO_PRICE: 50,
-
-  STORAGE_BUCKETS: {
-    SLIPS: "slips"
-  }
+  DEFAULT_PHOTO_PRICE: 50
 };
+
+/************************************************
+ * Global Supabase
+ ************************************************/
 
 window.sb = window.supabase.createClient(
   CONFIG.SUPABASE_URL,
   CONFIG.SUPABASE_ANON_KEY
 );
+
+/************************************************
+ * รองรับ code เดิม
+ ************************************************/
+
+window.supabaseClient = window.sb;
