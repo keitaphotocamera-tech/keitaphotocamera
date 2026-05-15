74fd101c8bc2b672fab3aeb484c84f6120b1b51d
keitaphotocamera-tech <keitaphotocamera@gmail.com>
2026-05-16 03:20:22 +0700

    Update config.js

--- photo-store/js/config.js
+++ photo-store/js/config.js
@@ -1,9 +1,16 @@
 window.CONFIG = {
   SUPABASE_URL: "https://ctmlmiiylgttbehyfjjd.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0bWxtaWl5bGd0dGJlaHlmampkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NTc5NjAsImV4cCI6MjA5NDQzMzk2MH0.O1EUG0MOGUZEL5WOVlZYS_hfUgSsGEzTUL69Nm1PWgo",
+
+  SUPABASE_ANON_KEY:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0bWxtaWl5bGd0dGJlaHlmampkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NTc5NjAsImV4cCI6MjA5NDQzMzk2MH0.O1EUG0MOGUZEL5WOVlZYS_hfUgSsGEzTUL69Nm1PWgo",
+
   LIFF_ID: "1661401303-Wld4uwIt",
 
-  DEFAULT_PHOTO_PRICE: 50
+  DEFAULT_PHOTO_PRICE: 50,
+
+  STORAGE_BUCKETS: {
+    SLIPS: "slips"
+  }
 };
 
 /************************************************
