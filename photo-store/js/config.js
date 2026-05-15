2b8ffe96bf5b234db03cb6d94483878ea080b5ed
keitaphotocamera-tech <keitaphotocamera@gmail.com>
2026-05-16 00:50:24 +0700

    Update config.js

--- photo-store/js/config.js
+++ photo-store/js/config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://ctmlmiiylgttbehyfjjd.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://ctmlmiiylgttbehyfjjd.supabase.co/";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0bWxtaWl5bGd0dGJlaHlmampkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NTc5NjAsImV4cCI6MjA5NDQzMzk2MH0.O1EUG0MOGUZEL5WOVlZYS_hfUgSsGEzTUL69Nm1PWgo";
 
 const supabaseClient = supabase.createClient(
