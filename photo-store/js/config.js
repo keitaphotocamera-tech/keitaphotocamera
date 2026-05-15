aa9e4aa6557a2972e0ab52a8f737c2cd6c78c9bb
keitaphotocamera-tech <keitaphotocamera@gmail.com>
2026-05-16 02:43:45 +0700

    Update config.js

--- photo-store/js/config.js
+++ photo-store/js/config.js
@@ -1,7 +1,16 @@
-const SUPABASE_URL = "https://ctmlmiiylgttbehyfjjd.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0bWxtaWl5bGd0dGJlaHlmampkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NTc5NjAsImV4cCI6MjA5NDQzMzk2MH0.O1EUG0MOGUZEL5WOVlZYS_hfUgSsGEzTUL69Nm1PWgo";
+window.CONFIG = {
+  SUPABASE_URL: "https://ctmlmiiylgttbehyfjjd.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0bWxtaWl5bGd0dGJlaHlmampkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NTc5NjAsImV4cCI6MjA5NDQzMzk2MH0.O1EUG0MOGUZEL5WOVlZYS_hfUgSsGEzTUL69Nm1PWgo",
+  LIFF_ID: "1661401303-Wld4uwIt",
 
-const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+  DEFAULT_PHOTO_PRICE: 50,
+
+  STORAGE_BUCKETS: {
+    SLIPS: "slips"
+  }
+};
+
+window.sb = window.supabase.createClient(
+  CONFIG.SUPABASE_URL,
+  CONFIG.SUPABASE_ANON_KEY
 );
